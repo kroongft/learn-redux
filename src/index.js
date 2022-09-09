3df0ede93b8fca095eb2ee25e66f638995f7e90e
krooooong <krooooong@naver.com>
2022-09-09 15:26:22 +0900

    feat: 리덕스 개발자 도구 설치 및 적용

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

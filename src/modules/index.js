27b7e1d10340cf1aa41d4fb6c98916900e234654
krooooong <krooooong@naver.com>
2022-09-09 14:58:20 +0900

    feat: 리덕스 모듈 생성, Provider 설정

---
+++ src/modules/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import counter from "./counter";
+import todos from "./todo";
+
+
+const rootReducer = combineReducers({
+  counter, 
+  todos
+});
+
+export default rootReducer;
